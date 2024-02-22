370a430cdd5dfcd2ac6fa54536979964cfac22ee
GabrielG-tech <gabriel@mpsouza.com.br>
2024-02-22 12:35:03 -0300

    Descricao

---
+++ Gabriel_Souza_PB_TP01/Prog_Tarefas.py
@@ -0,0 +1,17 @@
+# As tarefas a serem utilizadas poderão ter diferentes metadados: ID da tarefa, descrição, data de criação, status, prazo final, urgência, entre outros atributos... (O seu professor irá disponibilizar uma listagem de tarefas, bem como, as informações que serão manipuladas para cada uma das tarefas.)
+
+# Funcionalidades do Programa:
+
+# 1 - Adicionar Tarefa: Permitir ao usuário adicionar uma nova tarefa à lista de tarefas pendentes.
+# 2 - Listar Tarefas: Mostrar todas as tarefas pendentes na lista, enumerando-as.
+# 3 - Marcar Tarefa como Concluída: Permitir ao usuário marcar uma tarefa específica como concluída.
+# 4 - Remover Tarefa: Dar ao usuário a opção de remover uma tarefa da lista.
+
+# Detalhes da Implementação:
+
+# A - Utilize loops (for e/ou while) para apresentar um menu de opções ao usuário e realizar operações repetidas.
+# B - Manipule uma lista para armazenar e gerenciar as tarefas, incluindo adicionar, listar, marcar como concluída e remover tarefas.
+# C - Crie funções para cada funcionalidade do sistema (adicionar, listar, marcar como concluída, remover), utilizando argumentos, parâmetros por palavra-chave, parâmetros padrão e retorno de valores.
+# D - Documente cada função utilizando DocStrings para descrever seu propósito, uso e parâmetros.
+
+print("="*5 + "Lista de Tarefas" + "="*5)
