8d7ff3a75b101182246cd509d0aeec69eb201614
GabrielG-tech <gabriel@mpsouza.com.br>
2024-02-28 12:19:02 -0300

    Tarefas

--- Gabriel_Souza_PB_TP01/Prog_Tarefas.py
+++ Gabriel_Souza_PB_TP01/Prog_Tarefas.py
@@ -1,17 +1,60 @@
 # As tarefas a serem utilizadas poderão ter diferentes metadados: ID da tarefa, descrição, data de criação, status, prazo final, urgência, entre outros atributos... (O seu professor irá disponibilizar uma listagem de tarefas, bem como, as informações que serão manipuladas para cada uma das tarefas.)
 
-# Funcionalidades do Programa:
-
-# 1 - Adicionar Tarefa: Permitir ao usuário adicionar uma nova tarefa à lista de tarefas pendentes.
-# 2 - Listar Tarefas: Mostrar todas as tarefas pendentes na lista, enumerando-as.
-# 3 - Marcar Tarefa como Concluída: Permitir ao usuário marcar uma tarefa específica como concluída.
-# 4 - Remover Tarefa: Dar ao usuário a opção de remover uma tarefa da lista.
-
 # Detalhes da Implementação:
 
-# A - Utilize loops (for e/ou while) para apresentar um menu de opções ao usuário e realizar operações repetidas.
 # B - Manipule uma lista para armazenar e gerenciar as tarefas, incluindo adicionar, listar, marcar como concluída e remover tarefas.
 # C - Crie funções para cada funcionalidade do sistema (adicionar, listar, marcar como concluída, remover), utilizando argumentos, parâmetros por palavra-chave, parâmetros padrão e retorno de valores.
 # D - Documente cada função utilizando DocStrings para descrever seu propósito, uso e parâmetros.
 
-print("="*5 + "Lista de Tarefas" + "="*5)
+listaTarefas = [["Fazer Pizza", "Pendente"],
+                ["Estudar italiano", "Concluida"],
+                ["Jogar Uno", "Pendente"], 
+                ["TP01 - Projeto", "Pendente"]]
+
+def adicionar():
+    tarefa = input("Digite o nome da tarefa: ")
+    listaTarefas.append([tarefa, "Pendente"])
+
+def listar():
+    numTarefa = 0
+    for tarefa in listaTarefas:
+        numTarefa += 1
+        print("[{}] - {}\t - {}".format(numTarefa, tarefa[0], tarefa[1]))
+
+def concluir():
+    tarefa = int(input("Escolha a tarefa concluida: "))
+    listaTarefas[tarefa-1][1] = "Concluida"
+
+def remover():
+    tarefa = int(input("Digite o índice da tarefa: "))
+    listaTarefas.remove(listaTarefas[tarefa-1])
+
+continuar = True
+while continuar:
+    print("\n" + "="*8 + " Lista de Tarefas " + "="*8)
+    print("[1] - Adicionar Tarefa\n[2] - Listar Tarefas\n[3] - Marcar Tarefa como Concluída\n[4] - Remover Tarefa")
+    print("="*36 + "\n")
+    escolha = input("\nEscolha uma das opções: ")
+
+    if escolha == "1": 
+        adicionar()
+        # e = input("Deseja continuar? [s/n]: ").lower()
+        # if e == "n": 
+        #     continuar = False
+    elif escolha == "2":
+        listar()
+        # e = input("Deseja continuar? [s/n]: ").lower()
+        # if e == "n": 
+        #     continuar = False
+    elif escolha == "3":
+        concluir()
+        # e = input("Deseja continuar? [s/n]: ").lower()
+        # if e == "n": 
+        #     continuar = False
+    elif escolha == "4":
+        remover()
+        # e = input("Deseja continuar? [s/n]: ").lower()
+        # if e == "n": 
+        #     continuar = False
+    else:
+        print("Entrada inválida, tente novamente.\n")
